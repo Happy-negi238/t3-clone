d5e74d710056b1716a7a79ff687267a7c2e739d3
Happy-negi238 <happynegi238@gmail.com>
2026-08-25 12:48:03 +0530

    implement textarea and conversation

--- app/(root)/chat/[chatId]/page.tsx
+++ app/(root)/chat/[chatId]/page.tsx
@@ -1,7 +1,14 @@
+import {MessageViewWithForm} from "@/modules/chat/components/messages/message-view-form";
 import React from "react";
 
-const ChatPageId = () => {
-  return <div>ChatPageId</div>;
+const ChatPageId = async ({
+  params,
+}: {
+  params: Promise<{ chatId: string }>;
+}) => {
+  const {chatId} = await params;
+  return  <MessageViewWithForm chatId={chatId} />
+
 };
 
 export default ChatPageId;
